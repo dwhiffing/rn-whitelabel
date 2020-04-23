931566314a5bcd983fcb70f9dff6debdaff647a8
Daniel Whiffing <danielwhiffing@gmail.com>
2020-04-23 16:22:22 -0400

    basic two apps

--- App.js
+++ App.js
@@ -1,8 +1,23 @@
-import React, {Component} from 'react'
+import React from 'react'
 import {WebView} from 'react-native-webview'
+import {View, Text} from 'react-native'
+import Config from 'react-native-config'
 
 const App = () => {
-  return <WebView source={{uri: 'https://infinite.red'}} />
+  return (
+    <View style={{flex: 1}}>
+      <View
+        style={{
+          backgroundColor: Config.APP_COLOR,
+          alignItems: 'center',
+          padding: 20,
+          paddingTop: 50,
+        }}>
+        <Text style={{color: 'white'}}>{Config.APP_NAME}</Text>
+      </View>
+      <WebView source={{uri: 'https://infinite.red'}} />
+    </View>
+  )
 }
 
 export default App
